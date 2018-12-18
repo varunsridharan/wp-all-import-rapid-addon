96a88b381b3dc86f7247f7ee3f3f46ee0763d5a6
Varun Sridharan <varunsridharan23@gmail.com>
2018-12-18 14:33:48 +0530

    Create update.php

---
+++ update.php
@@ -0,0 +1,16 @@
+<?php
+$content = file_get_contents( 'https://raw.githubusercontent.com/soflyy/wp-all-import-rapid-addon/master/rapid-addon.php' );
+file_put_contents( 'rapid-addon.php', $content );
+
+date_default_timezone_set( 'UTC' );
+$output       = shell_exec( 'git log -1' );
+$build_number = getenv( 'TRAVIS_BUILD_NUMBER' );
+$gh_token     = getenv( 'GH_TOKEN' );
+
+echo shell_exec( "git checkout -f master
+git config --global user.email \"travis@travis-ci.org\"
+git config --global user.name \"Travis CI\"
+git add -A
+git commit -m \"Travis build: $build_number [skip ci]\"
+git remote set-url origin https://$gh_token@github.com/vsp-libs/wp-all-import-rapid-addon.git > /dev/null 2>&1
+git push origin master -f" );
