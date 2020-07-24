6f1db90abffa7bdd9a44dda76647056330f374c5
Varun Sridharan <varunsridharan23@gmail.com>
2020-07-24 20:54:24 +0530

    Update wpallimport.php

--- src/wpallimport.php
+++ src/wpallimport.php
@@ -6,14 +6,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
-use TheLeague\Database\Query_Builder;
+use RapidAddon;
 
 if ( ! class_exists( '\RapidAddon' ) ) {
-  require_once __DIR__.'/rapid-addon.php';
+	require_once __DIR__ . '/rapid-addon.php';
 }
 
 
 if ( ! class_exists( '\Varunsridharan\WordPress\WPAllImport' ) ) {
-  class WPAllImport extends RapidAddon{
-  }
+	/**
+	 * Class WPAllImport
+	 *
+	 * @package Varunsridharan\WordPress
+	 * @author Varun Sridharan <varunsridharan23@gmail.com>
+	 */
+	class WPAllImport extends RapidAddon {
+	}
 }
